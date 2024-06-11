748bfa94823027eebd0e2b936521fda6c3445dbe
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2024-06-11 16:05:59 +0700

    feat: add global error handling with ExceptionFilterAttribute

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,3 +1,4 @@
+using BuberDinner.Api.Filters;
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
 
@@ -5,7 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddApplication();
     builder.Services.AddInfrastructure(builder.Configuration);
-    builder.Services.AddControllers();
+    // builder.Services.AddControllers();
+
+    builder.Services.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
 }
 
 var app = builder.Build();
