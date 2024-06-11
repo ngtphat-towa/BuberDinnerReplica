d4e8e30b06e052fd5d761b39fd962095da6fc009
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2024-06-11 14:52:02 +0700

    refactor: add user into jwt services & add mapping entity to result

--- BuberDinner.Application/Authentication/Services/AuthenticationService.cs
+++ BuberDinner.Application/Authentication/Services/AuthenticationService.cs
@@ -2,6 +2,7 @@
 
 using BuberDinner.Application.Common.Services;
 using BuberDinner.Application.Persistence;
+using BuberDinner.Application.Mapping;
 using BuberDinner.Domain.Entities;
 
 namespace BuberDinner.Application;
@@ -25,24 +26,16 @@ public class AuthenticationService : IAuthenticationService
     public AuthenticationResult Login(string email, string password)
     {
         // validate existing of the user
-        var user = _userRepository.GetUserByEmail(email);
-        if (user is null || !user.Password.Equals(password))
+        var existingUser = _userRepository.GetUserByEmail(email);
+        if (existingUser is null || !existingUser.Password.Equals(password))
         {
             throw new Exception("Invalid user or password");
         }
 
         // Create Token
-        var token = _jwtTokenService.GenerateToken(
-            user.Id,
-            user.FirstName,
-            user.LastName);
+        var token = _jwtTokenService.GenerateToken(existingUser);
 
-        return new AuthenticationResult(
-            user.Id,
-            user.FirstName,
-            user.LastName,
-            email,
-            token);
+        return MappingExtensions.MapUserEntityToResult(existingUser, token);
     }
 
     public AuthenticationResult Register(string firstName, string lastName, string email, string password)
@@ -68,16 +61,11 @@ public class AuthenticationService : IAuthenticationService
         _userRepository.Add(user);
 
         // Create Token
-        var token = _jwtTokenService.GenerateToken(
-            userId,
-            firstName,
-            lastName);
+        var token = _jwtTokenService.GenerateToken(user);
 
-        return new AuthenticationResult(
-            userId,
-            firstName,
-            lastName,
-            email,
-            token);
+        return MappingExtensions.MapUserEntityToResult(user, token);
+
     }
+
+
 }
