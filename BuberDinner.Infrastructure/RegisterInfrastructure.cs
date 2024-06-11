b2308a7938fcf74545f2d696a20b480abdae24c3
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2024-06-11 12:13:34 +0700

    feat: add jwt setting and implementations

--- BuberDinner.Infrastructure/RegisterInfrastructure.cs
+++ BuberDinner.Infrastructure/RegisterInfrastructure.cs
@@ -1,12 +1,22 @@
+using BuberDinner.Application.Common.Services;
+using BuberDinner.Infrastructure.Authentication;
+using BuberDinner.Infrastructure.Common.Services;
+
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BuberDinner.Infrastructure;
 
 public static class RegisterInfrastructure
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
     {
+        services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SectionName));
 
+        services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
+        services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
         return services;
     }
 }
