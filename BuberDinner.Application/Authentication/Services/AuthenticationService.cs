73a20e6b38b25299464dd4a39018975ea58e2443
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2024-06-11 12:30:19 +0700

    Features/add jwt authentication (#2)
    * feat: add Jwt generator service
    
    * chore: moving jwt service to auth folder
    
    * feat: add date time provider
    
    * feat: add jwt setting and implementations
    
    * feat: init app settings
    
    * chore: use user-secrets to save jwt settings

--- BuberDinner.Application/Authentication/Services/AuthenticationService.cs
+++ BuberDinner.Application/Authentication/Services/AuthenticationService.cs
@@ -1,4 +1,8 @@
-﻿namespace BuberDinner.Application;
+﻿
+
+using BuberDinner.Application.Common.Services;
+
+namespace BuberDinner.Application;
 
 public interface IAuthenticationService
 {
@@ -7,13 +11,46 @@ public interface IAuthenticationService
 }
 public class AuthenticationService : IAuthenticationService
 {
+    private readonly IJwtTokenGenerator _jwtTokenService;
+
+    public AuthenticationService(IJwtTokenGenerator jwtTokenService)
+    {
+        _jwtTokenService = jwtTokenService;
+    }
+
     public AuthenticationResult Login(string email, string password)
     {
-        return new AuthenticationResult(Guid.NewGuid(), "firstName", "lastName", email, "token");
+
+        // TODO: validate existing of the user
+
+        // TODO: Create user 
+        var userId = Guid.NewGuid();
+        // Create Token
+        var token = _jwtTokenService.GenerateToken(
+            userId,
+            "firstName",
+            "lastName");
+
+        return new AuthenticationResult(Guid.NewGuid(), "firstName", "lastName", email, token);
     }
 
     public AuthenticationResult Register(string firstName, string lastName, string email, string password)
     {
-        return new AuthenticationResult(Guid.NewGuid(), firstName, lastName, email, "token");
+        // TODO: validate existing of the user
+
+        // TODO: Create user 
+        var userId = Guid.NewGuid();
+        // Create Token
+        var token = _jwtTokenService.GenerateToken(
+            userId,
+            firstName,
+            lastName);
+
+        return new AuthenticationResult(
+            userId,
+            firstName,
+            lastName,
+            email,
+            token);
     }
 }
