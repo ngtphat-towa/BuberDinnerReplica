314522e5e0c267029c5c72b811d6658f92197e9b
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2024-06-12 15:46:52 +0700

    Features/global error handling with custom problem details (#4)
    * feat: add error controller to handle exceptions
    
    * feat: add custom extension using ProblemDetailsFactory

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,15 +1,20 @@
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
 
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddApplication();
     builder.Services.AddInfrastructure(builder.Configuration);
     builder.Services.AddControllers();
+
+    builder.Services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>();
 }
 
 var app = builder.Build();
 {
+    app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
