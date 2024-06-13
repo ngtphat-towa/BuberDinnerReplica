ea974b0e162006e90efe842c9d762aff54880e9e
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2024-06-13 13:50:30 +0700

    Features/flow control via error or domain error (#5)
    * feat: define custom domain constants with error packages
    
    * refactor: using Error class in auth service
    
    * feat: add custom Problem with error constants & add problem details  the keys value

--- BuberDinner.Api/Controllers/ApiControllerBase.cs
+++ BuberDinner.Api/Controllers/ApiControllerBase.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BuberDinner.Api.Filters;
+
+using ErrorOr;
+
+using Microsoft.AspNetCore.Mvc;
 
 namespace BuberDinner.Api;
 
@@ -6,5 +10,20 @@ namespace BuberDinner.Api;
 [Route("[controller]")]
 public class ApiControllerBase : ControllerBase
 {
+    protected IActionResult Problem(List<Error> errors)
+    {
+        HttpContext.Items[HttpContextItemKeys.Errors] = errors;
 
+        var firstError = errors[0];
+
+        var statusCode = firstError.Type switch
+        {
+            ErrorType.Conflict => StatusCodes.Status409Conflict,
+            ErrorType.Validation => StatusCodes.Status400BadRequest,
+            ErrorType.NotFound => StatusCodes.Status404NotFound,
+            _ => StatusCodes.Status500InternalServerError
+        };
+
+        return Problem(statusCode: statusCode, title: firstError.Description);
+    }
 }
---
+++ BuberDinner.Api/Filter/HttpContextItemKeys.cs
@@ -0,0 +1,7 @@
+namespace BuberDinner.Api.Filters;
+
+public static class HttpContextItemKeys
+{
+    public const string Errors = "errors";
+    public const string ProblemDetailsErrors = "errorCodes";
+}
