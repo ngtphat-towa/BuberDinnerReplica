36bded5c1c52fb5cdeb5703394f9e14637c8f078
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2024-06-16 18:43:09 +0700

    refactor: replace with CQRS using MediatR

--- BuberDinner.Infrastructure/Persistence/UserRepository.cs
+++ BuberDinner.Infrastructure/Persistence/UserRepository.cs
@@ -7,13 +7,16 @@ public class UserRepository : IUserRepository
 {
     private static readonly List<User> _users = new();
 
-    public User? GetUserByEmail(string email)
+    public async Task<User?> GetUserByEmail(string email)
     {
+        await Task.CompletedTask;
         return _users.SingleOrDefault(u => u.Email == email);
     }
 
-    public void Add(User user)
+    public async Task Add(User user)
     {
+        await Task.CompletedTask;
+
         _users.Add(user);
     }
 }
