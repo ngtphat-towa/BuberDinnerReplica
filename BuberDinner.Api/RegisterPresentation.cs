1c14fca148409f060f209dc1832bfe47f37c7996
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2024-06-16 19:17:31 +0700

    Features/add object mapping with mapster

---
+++ BuberDinner.Api/RegisterPresentation.cs
@@ -0,0 +1,16 @@
+using BuberDinner.Api.Mappings;
+
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace BuberDinner.Api;
+
+public static class DependencyInjectionRegister
+{
+    public static IServiceCollection AddPresentation(this IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>();
+        services.AddMappings();
+        return services;
+    }
+}
