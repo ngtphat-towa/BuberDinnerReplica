677407dad1b85a0bb2edc19b3d7ff75dfda10def
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2024-06-18 06:59:03 +0700

    feat add menu reivew aggregate model

---
+++ BuberDinner.Domain/MenuReview/MenuReview.cs
@@ -0,0 +1,60 @@
+using BuberDinner.Domain.Common.Models;
+using BuberDinner.Domain.Common.ValueObjects;
+using BuberDinner.Domain.Dinner.ValueObjects;
+using BuberDinner.Domain.Guest.ValueObjects;
+using BuberDinner.Domain.Host.ValueObjects;
+using BuberDinner.Domain.Menu.ValueObjects;
+using BuberDinner.Domain.MenuReview.ValueObjects;
+
+namespace BuberDinner.Domain.MenuReview;
+
+public sealed class MenuReview : AggregateRoot<MenuReviewId>
+{
+    public MenuReview(
+        MenuReviewId menuReviewId,
+        Rating rating,
+        string comment,
+        HostId hostId,
+        MenuId menuId,
+        GuestId guestId,
+        DinnerId dinnerId,
+        DateTime createdDateTime = default,
+        DateTime updatedDateTime = default) :base(menuReviewId)
+    {
+        Rating = rating;
+        Comment = comment;
+        HostId = hostId;
+        MenuId = menuId;
+        GuestId = guestId;
+        DinnerId = dinnerId;
+        CreatedDateTime = createdDateTime;
+        UpdatedDateTime = updatedDateTime;
+    }
+
+    public Rating Rating { get; }
+    public string Comment { get; }
+    public HostId HostId { get; }
+    public MenuId MenuId { get; }
+    public GuestId GuestId { get; }
+    public DinnerId DinnerId { get; }
+    public DateTime CreatedDateTime { get; }
+    public DateTime UpdatedDateTime { get; }
+
+    public static MenuReview Create(
+         MenuReviewId menuReviewId,
+         Rating rating,
+         string comment,
+         HostId hostId,
+         MenuId menuId,
+         GuestId guestId,
+         DinnerId dinnerId) => new MenuReview(
+             MenuReviewId.CreateUnique(),
+             rating,
+             comment,
+             hostId,
+             menuId,
+             guestId,
+             dinnerId,
+             DateTime.UtcNow,
+             DateTime.UtcNow);
+}
