5555ecea7e4900e287ce0702589d125572e1e697
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2024-07-12 07:34:52 +0700

    feat(menu): Add CRUD operations for menus, sections, and items
    - Added endpoints for creating, retrieving, updating, and deleting menus
    - Implemented operations for managing menu sections and items within sections
    - Introduced request/response models for menu management

---
+++ BuberDinner.Api/Controllers/MenusController.cs
@@ -0,0 +1,54 @@
+﻿using BuberDinner.Contracts.Menu;
+using BuberDinner.Domain.Menu.ValueObjects;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberDinner.Api.Controllers;
+
+
+public class MenusController : ApiControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> CreateMenu([FromBody] CreateMenuRequest request)
+    {
+        await Task.CompletedTask;
+        return NoContent();
+
+    }
+
+    [HttpGet("{menuId}")]
+    public async Task<IActionResult> GetMenu(string menuId)
+    {
+        await Task.CompletedTask;
+
+        return NoContent();
+
+    }
+
+    [HttpPut("{menuId}/section/{sectionId}")]
+    public async Task<IActionResult> UpdateMenuSection(string menuId, string sectionId, [FromBody] UpdateMenuSectionRequest request)
+    {
+        await Task.CompletedTask;
+
+        return NoContent();
+
+    }
+
+    [HttpPut("{menuId}/section/{sectionId}/item/{itemId}")]
+    public async Task<IActionResult> UpdateMenuItem(string menuId, string sectionId, string itemId, [FromBody] UpdateMenuItemRequest request)
+    {
+        await Task.CompletedTask;
+
+        return NoContent();
+
+    }
+
+    [HttpDelete("{menuId}")]
+    public async Task<IActionResult> DeleteMenu(string menuId)
+    {
+        await Task.CompletedTask;
+
+        return NoContent();
+    }
+
+}
---
+++ BuberDinner.Contracts/Menu/MenuRequest.cs
@@ -0,0 +1,24 @@
+﻿namespace BuberDinner.Contracts.Menu;
+
+public record CreateMenuRequest(
+    string Name,
+    string Description,
+    List<MenuSectionTransfer> Sections);
+
+public record MenuSectionTransfer(
+    string Name,
+    string Description,
+    List<MenuItemTransfer> Items);
+
+public record MenuItemTransfer(
+    string Name,
+    string Description);
+
+public record UpdateMenuSectionRequest(
+    string Name,
+    string Description);
+
+public record UpdateMenuItemRequest(
+    string Name,
+    string Description);
+
