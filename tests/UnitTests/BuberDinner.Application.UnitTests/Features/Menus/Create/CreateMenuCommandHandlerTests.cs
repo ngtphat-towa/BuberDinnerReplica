7b17f6d2fb915b8ceb9648e50d79c9401aa8b8f4
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2024-07-16 07:37:28 +0700

    Features/add create menu unit test (#18)
    * chore: move source code to src directory
    
    * docs: add docs Api & domain models
    
    * feat: add Create Menu unit test

---
+++ tests/UnitTests/BuberDinner.Application.UnitTests/Features/Menus/Create/CreateMenuCommandHandlerTests.cs
@@ -0,0 +1,60 @@
+﻿using BuberDinner.Application.Features.Menus.Create;
+using BuberDinner.Application.Interfaces.Repositories;
+using BuberDinner.Application.UnitTests.Menus.Commands.TestUtils;
+using BuberDinner.Application.UnitTests.TestUtils.Menus.Extensions;
+using BuberDinner.Domain.Menus;
+
+using FluentAssertions;
+
+using Moq;
+
+namespace BuberDinner.Application.UnitTests.Features.Menus.Commands.CreateMenu
+{
+    public class CreateMenuCommandHandlerTests
+    {
+        private readonly Mock<IMenuRepository> _mockMenuRepository;
+        private readonly CreateMenuCommandHandler _handler;
+
+        public CreateMenuCommandHandlerTests()
+        {
+            _mockMenuRepository = new Mock<IMenuRepository>();
+            _handler = new CreateMenuCommandHandler(_mockMenuRepository.Object);
+        }
+
+        // T1: SUT - logical component we're testing (class, method)
+        // T2: Scenario - what we're testing
+        // T3: Expected outcome - where we expect the logical component to do
+        [Theory]
+        [MemberData(nameof(ValidCreateMenuCommands))]
+        public async Task HandleCreateMenuCommand_WhenMenuIsValid_ShouldCreateAndReturnMenu(
+            CreateMenuCommand command)
+        {
+            // Act
+            var result = await _handler.Handle(command, default);
+
+            // Assert
+            result.IsError.Should().BeFalse();
+            result.Value.ValidateCreatedFrom(command);
+            _mockMenuRepository.Verify(x => x.AddAsync(result.Value), Times.Once);
+        }
+
+        public static IEnumerable<object[]> ValidCreateMenuCommands()
+        {
+            yield return new[] { CreateMenuCommandUtils.CreateCommand() };
+
+            yield return new[]
+            {
+            CreateMenuCommandUtils.CreateCommand(
+                sections: CreateMenuCommandUtils.CreateMenuSectionCommand(3)),
+        };
+
+            yield return new[]
+            {
+            CreateMenuCommandUtils.CreateCommand(
+                sections: CreateMenuCommandUtils.CreateMenuSectionCommand(
+                    sectionsCount: 3,
+                    items: CreateMenuCommandUtils.CreateMenuItemCommand(5))),
+        };
+        }
+    }
+}
---
+++ tests/UnitTests/BuberDinner.Application.UnitTests/Features/Menus/Create/CreateMenuCommandUtils.cs
@@ -0,0 +1,32 @@
+﻿using BuberDinner.Application.Features.Menus.Create;
+using BuberDinner.Application.UnitTests.TestUtils.Constants;
+
+namespace BuberDinner.Application.UnitTests.Menus.Commands.TestUtils;
+
+public class CreateMenuCommandUtils
+{
+    public static CreateMenuCommand CreateCommand(
+        List<MenuSectionCommand>? sections = null) =>
+        new CreateMenuCommand(
+            FixtureConstants.Host.Id.Value,
+            FixtureConstants.Menus.Name,
+            FixtureConstants.Menus.Description,
+            sections ?? CreateMenuSectionCommand());
+
+    public static List<MenuSectionCommand> CreateMenuSectionCommand(
+        int sectionsCount = 1,
+        List<MenuItemCommand>? items = null) =>
+            Enumerable.Range(0, sectionsCount)
+                .Select(index => new MenuSectionCommand(
+                    FixtureConstants.Menus.SectionDescriptionFromIndex(index),
+                    FixtureConstants.Menus.SectionDescriptionFromIndex(index),
+                    items ?? CreateMenuItemCommand(sectionsCount)))
+                .ToList();
+
+    public static List<MenuItemCommand> CreateMenuItemCommand(int itemsCount = 1) =>
+        Enumerable.Range(0, itemsCount)
+            .Select(index => new MenuItemCommand(
+                FixtureConstants.Menus.ItemNameFromIndex(index),
+                FixtureConstants.Menus.ItemDescriptionFromIndex(index)))
+            .ToList();
+}
